4d9bb1ea2c84d20c82d59b9f881e0b1745f0a81a
Oriana Gomez <origds@gmail.com>
2013-03-14 22:58:58 -0430

    ya lee entradas, falta el main

--- sopaletra.pl
+++ sopaletra.pl
@@ -6,18 +6,31 @@
 %% Fecha: 22/03/13
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Funcion para cargar las listas de palabras
+
 cargarListaPalabra(Archivo, Alfabeto):-
 see(Archivo),
 read(Lista),
 seen,
-comparar(Lista,Alfabeto).
+transformaEnAscii(Lista,Alfabeto).
 
-comparar(Lista, Alfabeto):-
-atom_codes(Ascii,Alfabeto),
-display(get(Lista)),
-X == get(Lista),
-X \== 91,
-X \== 93,
-X \== 46,
-X \== 44,
-member(X,Ascii).
+% Funcion auxiliar para llevar las listas y el alfabeto a Ascii
+
+transformaEnAscii([],_).
+
+transformaEnAscii([C|R], Alf):-
+atom_codes(Temp,Alf),
+name(Temp, AlfabetoAscii),
+name(C, CabezaAscii),
+comparar(CabezaAscii,AlfabetoAscii),
+transformaEnAscii(R,Alf).
+
+% Funcion para comparar que las palabras estan formadas por
+% el alfabeto
+
+comparar([],_).
+
+comparar([C|R],Alf):-
+member(C,Alf),
+comparar(R,Alf).
+
